c063f590c6cbc8e83022c41ebcc70bd07e97541f
Jose A <jgallud@MacBook-Pro-de-admin.local>
2015-12-19 13:22:49 +0100

    esqueleto para dibujar la GUI

--- client/js/monopoly-client.js
+++ client/js/monopoly-client.js
@@ -1,7 +1,16 @@
 var url="http://127.0.0.1:1337/";
+var coord=[];
+var colorFicha=["red","blue","green","black","yellow","grey"];
+var fichas={lista:colorFicha}
+var posiciones=[];
 
 function inicio(){
+	cargarTablero();
+	cargarCoordenadas();
 	mostrarBotonPedirFicha();
+	
+	posiciones["red"]=20;
+	cargarFichas(1,ponerFicha);
 }
 
 //Funciones para modificar el index.html
@@ -20,6 +29,8 @@ function quitarBotonPedir(){
 function mostrarDatosJugador(nombre,uid){
 	mostrarNombre(nombre);
 	mostrarUid(uid);
+	posiciones["red"]=0;
+	cargarFichas(1,ponerFicha);
 }
 
 function mostrarNombre(nombre){
@@ -39,4 +50,66 @@ function obtenerFicha(nombre){
 		quitarBotonPedir();
 		mostrarDatosJugador(data.nombre,data.uid);
 	})
+}
+
+//funciones para dibujar el tablero y las fichas
+
+function cargarTablero(){
+	var canvas=document.getElementById("micanvas");
+	ctx=canvas.getContext("2d");
+	maxX=canvas.width;
+	maxY=canvas.height;
+	img=new Image();
+	img.src="client/img/tablero.png";
+	ctx.drawImage(img,0,0);
+	img.onload=function(){
+		ctx.drawImage(img,0,0);
+	}
+}
+
+function cargarFichas(numJug,callback){
+	var cont=0;
+	//numJug=parseInt($.cookie("numJug")); 
+	for(var i=0;i<numJug;i++){ //colorFicha.length
+		var color=colorFicha[i];
+		var imag=new Image();
+		imag.src="client/img/"+color+".png";
+		fichas.lista[color]=imag;
+		//fichas.posicion[color]=0;
+		ctx.drawImage(fichas.lista[color],maxX,maxY);
+		fichas.lista[color].onload=function(){
+			//ctx.drawImage(fichas.lista[color],maxX-70,maxY-70,30,30);
+			if (++cont>=numJug){
+				callback();
+			}
+		}
+	}	
+}
+
+function cargarCoordenadas(){
+	for(i=0;i<40;i++) coord[i]=[];
+	inc=55;
+	coord[0].push(maxX-inc*1.5)
+	coord[0].push(maxY-inc*1.5);
+
+	//coord[10].push(coord[9][0]-inc)
+	//coord[10].push(coord[9][1]);
+	
+	coord[20].push(inc)
+	coord[20].push(inc);
+
+	coord[30].push(maxX-inc)
+	coord[30].push(inc);
+}
+
+function ponerFicha(){
+	var x,y;
+	var color="red";
+	var posicion=posiciones[color];
+	console.log(color+" "+posicion);
+	if (posicion>=0 && posicion<40){
+		x=coord[posicion][0];
+		y=coord[posicion][1];
+		ctx.drawImage(fichas.lista[color],x,y,30,30);
+	}
 }
